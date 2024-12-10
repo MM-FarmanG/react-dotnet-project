bca5b8eead9daa44801daad9d105bfcee9bea6a5
Paras <pmehta@phillipjeffries.taravault.net>
2024-12-10 13:34:05 +0530

    Add project files.

---
+++ react-dotnet-project.client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Navbar from './Components/Navbar/Navbar'
+
+
+function App() {
+    return (
+            <Navbar />
+    );
+}
+
+export default App;
